dc060f41830d8b20bf8e05772f1996c7c179862e
judy-n <judynaamani@icloud.com>
2022-04-28 15:23:27 -0400

    feat: add "view more" for projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,5 @@
 import {
+    Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel,
     AspectRatio, Badge, Box,
     Button,
     Center,
@@ -17,6 +18,8 @@ import ProjectDesc from "../components/ProjectDesc";
 import four from "../img/four.png";
 import waw from "../img/waw.png"
 import linki from "../img/linki3.jpg"
+import health from "../img/health.jpg"
+import money from "../img/ms.png"
 
 function Projects() {
     return (
@@ -86,6 +89,91 @@ function Projects() {
                 />
 
             </Flex>
+
+
+            <Accordion
+                my={10}
+                allowToggle
+            borderStyle={'none'}
+            >
+                <AccordionItem
+                    borderStyle={'none'}
+                >
+                    {({ isExpanded }) => (
+                        <>
+                    <Center>
+                    <h2>
+                        <AccordionButton
+                        _hover={{ background: 'none'}}
+                        p={0}
+                        >
+                            <Button
+                                bg='#8B80F9'
+                                fontFamily={'fontSans'}
+                                fontWeight={500}
+                                _hover={{bg: '#776bea'}}
+                                _dark={{bg: '#3a6186',  color: 'white', _hover:{bg: '#2b547b'}}}
+                                rightIcon={<AccordionIcon />} colorScheme='purple' variant='solid'>
+                                {isExpanded ? 'Less Projects' : "More Projects"}
+                            </Button>
+
+                        </AccordionButton>
+                    </h2>
+                    </Center>
+                    <AccordionPanel
+                    p={0}>
+                        <Flex
+                            mt={20}
+                            display={{ lg: 'flex', }}
+                            spacing={'3rem'}
+                            flexDir={'row-reverse'}
+                            alignItems={'flex-start'}
+                            _light={{bgColor: 'gray.50', padding: 5, borderRadius:20}}
+                        >
+
+                            <ProjectCard
+                                style={{marginLeft: '2rem'}}
+                                img={health}
+                                link={'https://teambch.herokuapp.com/'}
+                                github={'https://github.com/JLambertazzo/healthkit'}
+                            />
+
+                            <Spacer m={'1rem'}/>
+                            <ProjectDesc
+                                title={'Health Kit'}
+                                desc={"Health Kit is a form creation web app that my team started in collaboration with Boston Children's Hospital. The goal is to create a " +
+                                    "solution for healthcare professionals to create or fill out forms that assess hospitals based on certain standards."}
+                                tags={['React','Chakra UI','JavaScript', 'Node.js']}
+                            />
+
+                        </Flex>
+                        <Flex
+                            mt={20}
+                            display={{ lg: 'flex', }}
+                            spacing={'3rem'}
+                            alignItems={'flex-start'}
+                            _light={{bgColor: 'gray.50', padding: 5, borderRadius:20}}
+                        >
+
+                            <ProjectCard
+                                noLink
+                                img={money}
+                                link={'https://www.producthunt.com/posts/linki'}
+                                github={'https://github.com/judy-n/MoneySaver'}
+                            />
+
+                            <ProjectDesc
+                                title={'Money Saver'}
+                                desc={'An iOS/Android app made to help users limit their spending. It keeps track of savings on would-be purchases.'}
+                                tags={['JavaScript','React Native']}
+                            />
+
+                        </Flex>
+                    </AccordionPanel>
+                        </>
+                        )}
+                </AccordionItem>
+            </Accordion>
         </Box>
     )
 
